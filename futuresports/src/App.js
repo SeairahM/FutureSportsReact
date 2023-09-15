46b0f86f2de073a5bbf41a0a98d62f217047920e
Sarah Marion <sarahomarion@gmail.com>
2023-09-15 20:46:17 +1000

    Photo Carousel

--- futuresports/src/App.js
+++ futuresports/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Navbar from './components/navbar';
 import './App.css';
+import PhotoCaraousel from './components/photocarousel';
 
 
 function App() {
@@ -8,33 +9,13 @@ function App() {
   return (
     <div className="App">
       <Navbar />
+      <PhotoCaraousel/>
       <div>
-        <h1 id="welcome">Welcome</h1>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
+        <h1 id="welcome">Future Sports</h1>
+        <p>Row your way, every day, with Future Sports.</p>
+        
         <h1 id="events">Events</h1>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
-        <p>This is the content of Section 2.</p>
+     
         <h1 id="about">Section 3</h1>
         <p>This is the content of Section 3.</p>
 
