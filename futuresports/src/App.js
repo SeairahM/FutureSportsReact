8025987760b438e1952109663beffa91f1930c66
Sarah Marion <sarahomarion@gmail.com>
2023-09-16 13:39:16 +1000

    Video and Facebook Page Section

--- futuresports/src/App.js
+++ futuresports/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/navbar';
 import './App.css';
 import PhotoCaraousel from './components/photocarousel';
 import Events from './components/events';
+import Media from './components/media';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
         <h1 id="welcome" style={{marginTop:'100px'}}>Future Sports</h1>
         <p>Row your way, every day, with Future Sports.</p>
         <Events/>
+        <Media/>
      
         <h2 id="about">About</h2>
         <p>This is the content of Section 3.</p>
