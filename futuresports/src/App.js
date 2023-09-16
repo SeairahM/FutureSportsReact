92427aa2be10b2d3e798e339e730d8ed81f1ef30
Sarah Marion <sarahomarion@gmail.com>
2023-09-16 13:07:37 +1000

    Event Sections

--- futuresports/src/App.js
+++ futuresports/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from './components/navbar';
 import './App.css';
 import PhotoCaraousel from './components/photocarousel';
+import Events from './components/events';
 
 
 function App() {
@@ -11,10 +12,9 @@ function App() {
       <Navbar />
       <PhotoCaraousel/>
       <div>
-        <h1 id="welcome">Future Sports</h1>
+        <h1 id="welcome" style={{marginTop:'100px'}}>Future Sports</h1>
         <p>Row your way, every day, with Future Sports.</p>
-        
-        <h2 id="events">Events</h2>
+        <Events/>
      
         <h2 id="about">About</h2>
         <p>This is the content of Section 3.</p>
