08cb793ba080a8a53a6934982eb4b62d9a4e6d5b
Sarah Marion <sarahomarion@gmail.com>
2023-09-17 11:13:30 +1000

    Contact Validation added

--- futuresports/src/App.js
+++ futuresports/src/App.js
@@ -7,6 +7,18 @@ import Media from "./components/media";
 import ContactForm from "./components/contact";
 
 function App() {
+  const aboutMsg = `Whether youre an experienced rower seeking a new 
+  imaginary challenge or a complete novice eager to dip your oars into the waters of
+  imagination, Future Sports Rowing Club welcomes you. We organize a
+  variety of events throughout the year, from friendly inter-club
+  regattas to workshops led by renowned rowing experts. These gatherings
+  not only enhance our members skills but also provide opportunities
+  for them to connect with fellow enthusiasts who share their passion.
+  Join us in creating a shared narrative of friendship, personal growth,
+  and the sheer joy of rowing across imaginary horizons. Your adventure
+  awaits on the waters of Future Sports.`
+
+
   return (
     <div className="App">
       <Navbar />
@@ -21,16 +33,7 @@ function App() {
 
         <h2 id="about" style={{marginTop:'100px'}}>About</h2>
         <p id="aboutp" style={{marginBottom:'100px'}}>
-          Whether you're an experienced rower seeking a new imaginary challenge
-          or a complete novice eager to dip your oars into the waters of
-          imagination, Future Sports Rowing Club welcomes you. We organize a
-          variety of events throughout the year, from friendly inter-club
-          regattas to workshops led by renowned rowing experts. These gatherings
-          not only enhance our members' skills but also provide opportunities
-          for them to connect with fellow enthusiasts who share their passion.
-          Join us in creating a shared narrative of friendship, personal growth,
-          and the sheer joy of rowing across imaginary horizons. Your adventure
-          awaits on the waters of Future Sports.{" "}
+          {aboutMsg}
         </p>
 
         <ContactForm />
