fe3dec2b969960a290acc4b0062ed9721c30599d
Sarah Marion <sarahomarion@gmail.com>
2023-09-18 13:26:09 +1000

    test removed

--- futuresports/src/index.js
+++ futuresports/src/index.js
@@ -6,7 +6,6 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <h1>Hello</h1>
     <App />
   </React.StrictMode>
 );
