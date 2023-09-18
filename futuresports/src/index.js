b1e2fee298dea396fe07eccefddad45af228116f
Sarah Marion <sarahomarion@gmail.com>
2023-09-18 13:05:15 +1000

    Test2

--- futuresports/src/index.js
+++ futuresports/src/index.js
@@ -6,6 +6,7 @@ import App from './App'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <h1>Hello</h1>
     <App />
   </React.StrictMode>
 );
